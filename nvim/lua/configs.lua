3dcd1664ef3ecf97c197175d65683d17fac1ec0a
facundoduhau <duhaufacundo@gmail.com>
2024-07-29 03:14:42 -0300

    Dejo solo Kitty y Neovim | Cambios a config de Neovim

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -1,6 +1,6 @@
 -- Numbering
   vim.cmd("set number")
-  vim.cmd("set relativenumber")
+  -- vim.cmd("set relativenumber")
 
 -- Leader key, and netrw file
   vim.g.mapleader = ' '
@@ -12,3 +12,6 @@
   vim.cmd("set softtabstop=2")
   vim.cmd("set shiftwidth=2")
 
+-- Folding
+  vim.cmd("set foldmethod=manual")
+  vim.cmd("set foldcolumn=1")
